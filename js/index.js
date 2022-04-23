a7eece80e4ab8059f5fb591d21e933c9a421e224
Giru <yagofarov.vadim@mail.ru>
2022-04-23 21:14:42 +0500

    fix obj card

--- js/index.js
+++ js/index.js
@@ -139,13 +139,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	window.addEventListener('scroll', showModalByScroll);
 
 	class MenuCard {
-		constructor(src, alt, title, desc, price, parentSelector) {
+		constructor(src, alt, title, desc, price, parentSelector, ...classes) {
 			this.src      = src;
 			this.alt      = alt;
 			this.title    = title;
 			this.desc     = desc;
 			this.price    = price;
-			this.parent    = document.querySelector(parentSelector);
+			this.classes  = classes;
+			this.parent   = document.querySelector(parentSelector);
 			this.transfer = 81;
 			this.changeToUAH();
 		}
@@ -156,8 +157,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+			if (!this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element)
+			} else {
+				this.classes.forEach(className => element.classList.add(className))
+			}
+			
 			element.innerHTML = `
-			<div class="menu__item">
 				<img src="${this.src}" alt="${this.alt}">
 				<h3 class="menu__item-subtitle">${this.title}</h3>
 				<div class="menu__item-descr">${this.desc}</div>
@@ -165,8 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
 				<div class="menu__item-price">
 					<div class="menu__item-cost">Цена:</div>
 					<div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-				</div>
-			</div>`;
+				</div>`;
 			this.parent.append(element);
 		}
 	}
@@ -177,7 +183,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		'Меню "Фитнес"',
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		2,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 
 	new MenuCard(
@@ -186,7 +193,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		'Меню "Фитнес"',
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		2,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 
 	new MenuCard(
@@ -195,7 +203,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		'Меню "Фитнес"',
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		2,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 	
 });
