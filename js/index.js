9a2c8ca6b35c41906fcaf542a2070958d317765f
Giru <yagofarov.vadim@mail.ru>
2022-08-24 15:07:53 +0500

    add create new data in file

--- js/index.js
+++ js/index.js
@@ -182,32 +182,22 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 	
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"vegy",
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		".menu .container"
-	).render();
+	const getResource = async (url) => {
+		const res = await fetch(url);
+		
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+		
+		return await res.json();
+	}
 	
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"post",
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		14,
-		".menu .container"
-	).render();
-	
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"elite",
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		21,
-		".menu .container"
-	).render();
+	getResource('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(({img, altimg, title, descr, price}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		});
 	
 	// Forms
 	
@@ -219,10 +209,24 @@ window.addEventListener('DOMContentLoaded', function() {
 	};
 	
 	forms.forEach(item => {
-		postData(item);
+		bindPostData(item);
 	});
 	
-	function postData(form) {
+	// отвечает за постинг данных, получает запрос и получает данные
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+		
+		return await res.json();
+	}
+	
+	// отвечает за привязку постинга
+	function bindPostData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
 			
@@ -234,29 +238,21 @@ window.addEventListener('DOMContentLoaded', function() {
             `;
 			form.insertAdjacentElement('afterend', statusMessage);
 			
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-			request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 			const formData = new FormData(form);
 			
-			const object = {};
-			formData.forEach(function(value, key){
-				object[key] = value;
-			});
-			const json = JSON.stringify(object);
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 			
-			request.send(json);
-			
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					statusMessage.remove();
-					form.reset();
-				} else {
-					showThanksModal(message.failure);
-				}
-			});
+			postData('http://localhost:3000/requests', json)
+			.then(data => data.text())
+			.then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				statusMessage.remove();
+			}).catch(() => {
+				showThanksModal(message.failure);
+			}).finally(()=>{
+				form.reset();
+			})
 		});
 	}
 	
@@ -282,4 +278,8 @@ window.addEventListener('DOMContentLoaded', function() {
 			closeModal();
 		}, 4000);
 	}
+	
+	fetch('http://localhost:3000/menu', )
+		.then(data => data.json())
+		.then(res => console.log(res))
 });
